db385fe6b2904aa7e9dfeaa487d10ce7fc916273
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-05-01 10:47:01 -0700

    fixed start command

--- middleware/data/club-members.ts
+++ middleware/data/club-members.ts
@@ -16,9 +16,16 @@ clubMemberRouter.get('/:club_id', async (req: Request, res: Response) => {
     }
 })
 
-clubMemberRouter.post('/:club_id', async (req: Request, res: Response) => {
+const examplePostClubMember = {
+    userId: 3,
+    clubId: 1
+}
+
+//since club member data is derived from user data, no need to prevent existing user because already exists
+clubMemberRouter.post('/', async (req: Request, res: Response) => {
     try {
-        const { rows } = await db.query('INSERT INTO club_members SELECT * FROM vw_club_members WHERE club_id = $1', [req.params.club_id])
+        const newClubMember = Object.values(req.body)
+        const { rows } = await db.query('INSERT INTO club_members (user_id, club_id) VALUES ($1, $2);', newClubMember)
         res.status(200).send(rows)
     }
     catch(err) {
